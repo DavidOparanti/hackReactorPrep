1694fea150d99c79225005802b3582466cddecb2
David Oparanti <david.oparanti@gmail.com>
2018-07-10 23:33:38 +0100

    added modulo 0

---
+++ mudulo0/transformEmployeeData.js
@@ -0,0 +1,40 @@
+/*
+Write a function called "transformEmployeeData" that transforms some employee data from one format to another.
+
+The argument will look something like this:
+[
+    [
+        ['firstName', 'Joe'], ['lastName', 'Blow'], ['age', 42], ['role', 'clerk']
+    ],
+    [
+        ['firstName', 'Mary'], ['lastName', 'Jenkins'], ['age', 36], ['role', 'manager']
+    ]
+]
+
+Given that input, the return value should look like this:
+[
+    {firstName: 'Joe', lastName: 'Blow', age: 42, role: 'clerk'},
+    {firstName: 'Mary', lastName: 'Jenkins', age: 36, role: 'manager'}
+]
+
+Note that the input may have a different number of rows or different keys than the given sample. 
+
+For example, let's say the HR department adds a "tshirtSize" field to each employee record. Your code should flexibly accommodate that.
+
+Starter Code :
+*/
+
+function transformEmployeeData(array) {
+  // your code here
+  const arr = [];
+  const obj = {};
+  array.forEach(a => {
+      for(let i = 0; i < a.length; i++) {
+          obj[a[i][0]] = a[i][1];
+      }
+      arr.push(obj)
+  })
+
+  return arr;
+}
+
